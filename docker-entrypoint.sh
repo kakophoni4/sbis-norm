5987c25f349f364ed5349a16afe7edfb35b342ca
kakophoni4 <kakophoni4@users.noreply.github.com>
2026-09-25 14:39:18 +0400

    Persist CryptoPro license across container recreation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,6 @@ mkdir -p /var/opt/cprocsp/keys/root
 chmod 1777 /var/opt/cprocsp/keys
 chmod 700 /var/opt/cprocsp/keys/root
 
-/opt/cprocsp/sbin/amd64/cpconfig -license -set 00000-00000-00000-00000-00000 2>/dev/null || true
 /opt/cprocsp/sbin/amd64/cpconfig -ini '\config\random' -add string '/dev/urandom' 2>/dev/null || true
 
 if [ -f /app/scripts/ops/install_certificates.py ]; then
